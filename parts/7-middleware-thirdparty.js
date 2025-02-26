1503a8601c76d1181098e1eba849304afb0e7a16
Leandro Guandalini <leandro.gribeiro@al.infnet.edu.br>
2025-02-26 10:37:53 -0300

    thirdparty middleware example

---
+++ parts/7-middleware-thirdparty.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const app = express()
+const morgan = require('morgan');
+const logger = require('./logger')
+const authorize = require('./authorize')
+//req => middleware => res
+
+//coloca o middleware global, sem precisar ficar escrevendo em cada uma o logger (bom para algo que precisa estar presente em todas)
+//o use só funciona para baixo, se tiver alguma função acima ele não ira entrar
+//se passar um caminho, só será aplicado nos que começarem com esse path
+
+// app.use([logger, authorize])
+
+app.use(morgan('tiny'))
+
+app.get('/', (req,res) => {
+  res.send('Home')
+})
+
+app.get('/about', (req,res) => {
+  res.send('About')
+})
+
+app.get('/api/products', (req,res) => {
+  res.send('Products')
+})
+
+app.get('/api/items', (req,res) => {
+  console.log(req.user);
+  res.send('Items')
+})
+
+app.listen(5000, () => {
+  console.log('Server is listening on port 5000');
+  
+})
